a460097c479a288f704cda2f4341a20b5a9dfb9d
Dominique Maucieri <dominiquemaucieri@gmail.com>
2022-03-11 15:08:50 -0800

    adding environmental data

--- data-raw/add_coral_data.R
+++ data-raw/add_coral_data.R
@@ -23,3 +23,10 @@ coral_labelset <- read.csv(here::here("data-raw", "coral_labelset.csv"))
 
 usethis::use_data(coral_labelset, overwrite = TRUE,
                   compress = "xz")
+
+
+environmental_data <- read.csv(here::here("data-raw", "environmental_data.csv"))
+
+usethis::use_data(environmental_data, overwrite = TRUE,
+                  compress = "xz")
+
